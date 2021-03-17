d3dce3602376990be4a8086afe4ca80b0b8c702b
zxy <zcbtyz@qq.com>
2021-03-17 11:09:52 +0800

    first commit.

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    repositories {
+        mavenLocal()
+        maven("https://plugins.gradle.org/m2/")
+        mavenCentral()
+        jcenter()
+    }
+
+    val kotlinVersion = "1.4.21"
+
+    dependencies {
+        classpath(kotlin("allopen", kotlinVersion))
+        classpath(kotlin("noarg", kotlinVersion))
+        classpath(kotlin("gradle-plugin", kotlinVersion))
+    }
+}
+
+allprojects {
+    group = "xyz.starestarrysky"
+    version = "1.0.0"
+
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        jcenter()
+    }
+
+    dependencies {
+        val implementation by configurations
+        val testImplementation by configurations
+
+        implementation((kotlin("reflect")))
+        implementation((kotlin("stdlib")))
+        testImplementation((kotlin("test")))
+        testImplementation((kotlin("test-junit")))
+    }
+
+    configure<SourceSetContainer> {
+        named("main") {
+            java.srcDir("src/main/kotlin")
+        }
+        named("test") {
+            java.srcDir("src/test/kotlin")
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "15"
+        }
+    }
+}
+
+tasks.named<Wrapper>("wrapper") {
+    gradleVersion = "6.8.3"
+    distributionType = Wrapper.DistributionType.BIN
+}
